2b782a03893544956c7b29ed1176f08d39806b22
ConnellHagen <hagen.connell@gmail.com>
2025-09-23 08:51:12 -0500

    idk

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route('/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.post('/submitForm')
+def submit():
+    text = request.args.get('thebox')
+    print(request.args)
+    return render_template('hello.html')
