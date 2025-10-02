4c396b4ff4acd4323d9cee207518ded2f298507f
ConnellHagen <hagen.connell@gmail.com>
2025-10-02 08:27:59 -0500

    Add grail layout

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session
 from database import *
 
 app = Flask(__name__)
+app.secret_key = os.environ['FLASK_SECRET']
 
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
     guests = get_guests()
     return render_template('hello.html', name=name, guests=guests)
+
+@app.route('/grail')
+def grail():
+    return render_template('grail.html')
 
 @app.post('/submitForm')
 def submit():
